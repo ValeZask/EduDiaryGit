40695fe55d72185fe5b3be155a18dd20327eed9e
ТВОЁ_ИМЯ_В_GIT <_EMAIL>
2025-05-03 00:30:32 +0600

    Page News is ready

--- edu_diary/school/news/urls.py
+++ edu_diary/school/news/urls.py
@@ -7,6 +7,7 @@ app_name = 'news'
 urlpatterns = [
     path('categories/', CategoryViewSet.as_view({'get': 'list', 'post': 'create'}), name='category-list-create'),
     path('categories/<int:pk>/', CategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='category-detail'),
+    
     path('list/', NewsListView.as_view(), name='news-list'),
     path('create/', NewsCreateView.as_view(), name='news-create'),
 ]
