0e2bf180e720f51e8580059a0df20af71e98db6e
ChynNiet <nietbekovchyngyz@gmail.com>
2025-05-05 21:25:32 +0600

    final test

--- edu_diary/school/admin.py
+++ edu_diary/school/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .homepage.models import Project, ProjectMember, ProjectTask, Event, StudentEvent
 from .diary.models import Class, Subject, Schedule, Grade
-from .news.models import News
+from .news.models import News, Category
 from .achievements.models import Achievement, AchievementPlace, AchievementCategory
+from .chat.models import Chat, ChatMessage, ChatParticipant
 
 admin.site.register(StudentEvent)
 admin.site.register(Project)
@@ -17,3 +18,9 @@ admin.site.register(News)
 admin.site.register(Achievement)
 admin.site.register(AchievementPlace)
 admin.site.register(AchievementCategory)
+admin.site.register(Chat)
+admin.site.register(ChatMessage)
+admin.site.register(ChatParticipant)
+admin.site.register(Category)
+
+
